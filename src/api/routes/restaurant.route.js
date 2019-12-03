473ba7d285c849dedc2b14f1248118fee6cdc67d
Kevin Carter <9323jkc@gmail.com>
2019-12-03 06:06:40 -0700

    added the permission to restaurant route

--- src/api/routes/restaurant.route.js
+++ src/api/routes/restaurant.route.js
@@ -3,8 +3,12 @@ import restaurantCtrl from '../controllers/restaurant.controller'
 
 const router = express.Router();
 
+router.use(policies.checkRoles([ROLES.ADMIN, ROLES.OWNER]));
+
 router.route('/')
-  .get(restaurantCtrl.list)
-  .post(restaurantCtrl.create);
+    .get(restaurantCtrl.list)
+    .post([policies.checkRoles([ROLES.ADMIN, ROLES.OWNER]), restaurantCtrl.create]);
+
 
 export default router;
+
