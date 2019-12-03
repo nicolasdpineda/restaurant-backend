696895dc07ab805e4ccec9d40830facd90831210
Kevin Carter <9323jkc@gmail.com>
2019-12-03 03:00:31 -0700

    Add two models(Restaurant and Review) on <restaurant.model.js>

--- src/api/models/restaurant.model.js
+++ src/api/models/restaurant.model.js
@@ -1,19 +1,30 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
-const positiveNumber = (distance) => distance > 0;
+const rateValidation = (rate) => (rate => 0 && rate <= 5);
 
-const Restaurant = new Schema({
-  date: {
-    type: Date,
-  },
-  rate: {
-    type: Number,
-    default: 0,
-    validate: [positiveNumber, 'Rate should be bigger than 00:00'],
-  },
+const RestaurnatShema = new Schema({
+  name  : { type: String, required: true },
+  user  : { type: Schema.ObjectId,  ref: 'User' },
 }, {
   timestamp: true,
 });
 
-module.exports = mongoose.model('Entry', EntrySchema);
+
+const ReviewSchmea = new Schema({
+  rate  : { type: Number, required: true, validate: [rateValidation, 'Rate should be between 0 - 5'] },
+  comment : { type: String, required: true },
+  restaurant: { type: Schema.ObjectId, ref: 'Restaurant'},
+  user: { type: Schema.ObjectId, ref: 'User' },
+}, {
+  timestamp: true,
+});
+
+
+const Restaurant = mongoose.model('Restaurant', RestaurnatShema);
+const Review = mongoose.model('Review', ReviewSchmea );
+
+export default { 
+  Restaurant,
+  Review
+}
