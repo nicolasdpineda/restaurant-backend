681e63bd772cc2cad75556320b5c17b7270b2b78
Kevin Carter <9323jkc@gmail.com>
2019-12-03 03:20:29 -0700

    add <restaurant.controller.js> file

---
+++ src/api/controllers/restaurant.controller.js
@@ -0,0 +1,36 @@
+import User from "../models/user.model";
+import Restaurant from '../models/restaurant.model'
+
+import ROLES from "../constants/role";
+
+
+function create(req, res, next) {
+  const restaurant = new Restaurant({
+    name: req.body.name,
+    user: req.user._id,
+  });
+
+  restaurant
+    .save()
+    .then(newRestaurant => {
+      res.json(newRestaurant);
+    })
+    .catch(next);
+}
+
+
+function list(req, res, next) {
+  let where = {};
+
+  Restaurant.find(where)
+    .then(restaurants => {
+      res.json(restaurants);
+    })
+    .catch(next);
+
+}
+
+export default {
+  create,
+  list,
+};
