c236ec3e6e5760950a7945297bda104591183a78
Kevin Carter <9323jkc@gmail.com>
2019-12-03 06:36:15 -0700

    Fixed the import polices issue on restaurant.route.js

--- src/api/models/review.model.js
+++ src/api/models/review.model.js
@@ -1,3 +1,5 @@
+import { SchemaType, mongo } from 'mongoose';
+
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
@@ -5,12 +7,25 @@ const rateValidation = (rate) => (rate => 0 && rate <= 5);
 
 const ReviewSchmea = new Schema({
   rate  : { type: Number, required: true, validate: [rateValidation, 'Rate should be between 0 - 5'] },
-  comment : { type: String, required: true },
+  text  : { type: String, required: true },
   restaurant: { type: Schema.ObjectId, ref: 'Restaurant'},
   user: { type: Schema.ObjectId, ref: 'User' },
+  comment: { type: Schema.ObjectId, ref: 'Commnet'}
 }, {
   timestamp: true,
 });
 
+const CommentSchema = new Schema({
+    text: { type: String, required: true },
+    user: { type: Schema.ObjectId, ref: 'User'}
+}, {
+    timestamps: true,
+})
 
-export default mongoose.model('Review', ReviewSchmea );
+const Review = mongoose.model('Review', ReviewSchmea);
+const Commnet = mongoose.model('Commnet', CommentSchema);
+
+export default {
+    Review,
+    Commnet
+};
--- src/api/routes/restaurant.route.js
+++ src/api/routes/restaurant.route.js
@@ -1,6 +1,8 @@
 import express from 'express';
 import restaurantCtrl from '../controllers/restaurant.controller'
 
+import policies from '../policies';
+
 const router = express.Router();
 
 router.use(policies.checkRoles([ROLES.ADMIN, ROLES.OWNER]));
@@ -8,7 +10,6 @@ router.use(policies.checkRoles([ROLES.ADMIN, ROLES.OWNER]));
 router.route('/')
     .get(restaurantCtrl.list)
     .post([policies.checkRoles([ROLES.ADMIN, ROLES.OWNER]), restaurantCtrl.create]);
-
 
 export default router;
 
